2929e1152160f9d13bfcbd06044357b00c319a68
Izaaz Kothawala <ikothawa@hawk.iit.edu>
2017-02-08 13:15:12 -0600

    adding base index.html file

---
+++ js/Script.js
@@ -0,0 +1,14 @@
+
+          function addtext()
+          {
+             var fname = document.myform.first_name.value;
+             var lname = document.myform.last_name.value;
+             var email = document.myform.email.value;
+
+             document.writeln("Thank you! You are registered with the following details:");
+             document.writeln("<pre>");
+             document.writeln("First Name    : " + fname);
+             document.writeln("Last Name     : " + lname);
+             document.writeln("Email Address : " + email);
+          }
+  
